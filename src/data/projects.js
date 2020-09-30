43daa1879c1922ddc46ab408b50ecfc01b20b62c
Grzegorz Kaczor <grzesiek125@op.pl>
2020-09-30 12:59:05 +0200

    about me & projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,11 @@
 const projects = [
-    {id:1, title: 'Weather App', img: "https://github.com/Grzk8/first-rep/blob/master/weatherapp.png", desctiption: "Aplikacja pogodowa. Wpisz nazwę miasta, aby sprawdzić aktualną pogodę. Dane pogodowe pobierane są ze strony: https://openweathermap.org", adressLive: "https://grzk8.github.io/Weather-App/", adressGithub: "https://github.com/Grzk8/Weather-App"},
-    {id:2, title: 'Furry Game', img: "https://github.com/Grzk8/first-rep/blob/master/furrygame.png", desctiption: "Prosta gra przeglądarkowa. Celem gry jest zbieranie monetw celu zdobycia punktów", adressLive: "https://grzk8.github.io/Furry-Game/", adressGithub: "https://github.com/Grzk8/Furry-Game"},  
+
+    {id:6, title: 'Portfolio', img: "Moja strona portfolio", desctiption: "", adressLive: "", adressGithub: ""},
+    {id:5, title: 'Weather App', img: "https://raw.githubusercontent.com/Grzk8/Portfolio/master/src/images/weatherapp.png", desctiption: "Aplikacja pogodowa. Wpisz nazwę miasta, aby sprawdzić aktualną pogodę. Dane pogodowe pobierane są ze strony: https://openweathermap.org", adressLive: "https://grzk8.github.io/Weather-App/", adressGithub: "https://github.com/Grzk8/Weather-App"},
+    {id:4, title: 'Furry Game', img: "https://raw.githubusercontent.com/Grzk8/Portfolio/master/src/images/furrygame.png", desctiption: "Prosta gra przeglądarkowa. Celem gry jest zbieranie monetw celu zdobycia punktów", adressLive: "https://grzk8.github.io/Furry-Game/", adressGithub: "https://github.com/Grzk8/Furry-Game"},
+    {id:3, title: 'BestBookstore', img: "https://raw.githubusercontent.com/Grzk8/Bookstore/dev/BestBooKStore2.png", desctiption: "Strona księgarni internetowej. Informacje o książkach pobierane są z pliku JSON z zewnętrznego serwera. Tam także wysyłane są informcje z formularza po złorzeniu zamówienia", adressLive: "", adressGithub: "https://github.com/Grzk8/Bookstore"},
+    {id:2, title: 'Oddaj Rzeczy', img: "https://raw.githubusercontent.com/Grzk8/Portfolio/master/src/images/oddajrzeczy.png", desctiption: "", adressLive: "", adressGithub: ""},
+    {id:1, title: 'Bestshop', img: "https://raw.githubusercontent.com/Grzk8/Portfolio/master/src/images/bestshop.png", desctiption: "Strona internetowa platformy sprzedażowej", adressLive: "", adressGithub: "https://github.com/Grzk8/Bookstore"},
     ]
 
 export default projects;
