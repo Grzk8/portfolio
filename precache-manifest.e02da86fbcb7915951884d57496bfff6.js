ec99d1638a04b48be4d56eb301447f74ae03f0e2
Grzegorz Kaczor <grzesiek125@op.pl>
2021-01-06 13:44:54 +0100

    Updates

---
+++ precache-manifest.e02da86fbcb7915951884d57496bfff6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b4c02d20075d72ab8fa9d4b94a82b1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "32261c73986b09e68126",
+    "url": "/portfolio/static/css/main.0e6a3be3.chunk.css"
+  },
+  {
+    "revision": "6bd991dac9f0da960560",
+    "url": "/portfolio/static/js/2.80a93393.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.80a93393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32261c73986b09e68126",
+    "url": "/portfolio/static/js/main.37ee01fc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "3c8b72516080053e4d675d5abb8e7398",
+    "url": "/portfolio/static/media/fascinate-regular-webfont.3c8b7251.woff2"
+  },
+  {
+    "revision": "c14af0a4d79575883a8497c083a32e13",
+    "url": "/portfolio/static/media/fascinate-regular-webfont.c14af0a4.woff"
+  },
+  {
+    "revision": "9898c37489052ad7d42931336e1ed71f",
+    "url": "/portfolio/static/media/reeniebeanie-regular.9898c374.woff2"
+  },
+  {
+    "revision": "9f449072e5d72109ea70eb0f3bf5b734",
+    "url": "/portfolio/static/media/reeniebeanie-regular.9f449072.woff"
+  }
+]);
