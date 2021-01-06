24fdc71e85225ff474e5bcd7b760c608b81752c2
Grzegorz Kaczor <grzesiek125@op.pl>
2021-01-06 13:41:49 +0100

    update

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -15,12 +15,11 @@ function About() {
             <p className={'about_text'}>Postanowiłem jednak kształcić się w kierunku web developmentu.
             Programowaniem zajmuję się od półtora roku. Swoje projekty tworzyłem głównie za pomocą React w kierunku którego chciałbym się dalej rozwijać.
             Jednocześnie uczę się także technologii umożliwiających budowanie aplikacji po stronie serwera: NodeJS, Express.js, SQL, MVC, NoSQL, GraphQL, Deno.js.
-            Staram się używać poprawnego i semantycznego kodu HTML i CSS. Korzystam ze składni preprocesora Sass.
+            Staram się używać poprawnego i semantycznego kodu HTML i CSS, korzystam ze składni preprocesora Sass.
             Ukończyłem kursy: JavaScript Developer: React organizowany przez szkołę programowania CodersLab 
             oraz kurs Udemy – React - The Complete Guide (incl Hooks, React Router, Redux) Academind by Maximilian Schwarzmüller.
             </p>
             <p className={'about_text'}>Szukam pierwszej pracy jako Front-end Developer w której będę mógł wykorzystać zdobytą wiedzę, oraz pomoże mi w dalszym rozwoju. 
-            Staram się używać poprawnego i semantycznego kodu HTML i CSS. Korzystam ze składni preprocesora Sass.
             </p>
             </section>
         </>
