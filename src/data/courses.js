38baa7039c41a67c2321d4af7b13acfc0c01445c
Grzegorz Kaczor <grzesiek125@op.pl>
2021-07-08 13:02:45 +0200

    courses pics

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,6 +1,6 @@
 const courses = [
-    {id:1, pic: "", name: "JavaScript Developer: React - CodersLab"},
-    {id:2, pic: "", name: "Udemy – React - The Complete Guide (incl Hooks, React Router, Redux) Academind by Maximilian Schwarzmüller"},
+    {id:1, pic: "https://raw.githubusercontent.com/Grzk8/Portfolio/dev/src/images/courses/courses_small_1.jpg", name: "JavaScript Developer: React - CodersLab"},
+    {id:2, pic: "https://raw.githubusercontent.com/Grzk8/Portfolio/dev/src/images/courses/courses_small_2.jpg", name: "Udemy – React - The Complete Guide (incl Hooks, React Router, Redux) Academind by Maximilian Schwarzmüller"},
     ]
 
 export default courses;
