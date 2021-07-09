c42029b081d471d12fb05ec52a656b92d9b7e66e
Grzegorz Kaczor <grzesiek125@op.pl>
2021-07-09 11:44:56 +0200

    courses

--- src/js/components/main/Courses.js
+++ src/js/components/main/Courses.js
@@ -7,13 +7,14 @@ class Courses extends Component{
         console.log(this.props.courses)
         return<>
             <section className={'container'} id='courses'>
-                <p className={'title'}>Kursy</p>
+                <p className={'title'}>Kursy i certyfikaty</p>
                 <div className={'courses_cont'}>
                     {this.props.courses.map (courses=>
                             <Course 
                                 id={courses.id}
                                 pic={courses.pic} 
                                 name={courses.name}
+                                organizer={courses.organizer}
                                 link={courses.link}/>
                             )}
                 </div>
--- src/js/components/main/main_components/Course.js
+++ src/js/components/main/main_components/Course.js
@@ -4,10 +4,11 @@ const course = props => {
     return (
         <>
         <div className='course' key={props.id}>
-            <a href={props.link}>
-                <img src={props.pic} className="course_small" target="_blank"/>
+            <a href={props.link} target="_blank">
+                <img src={props.pic} className="course_small"/>
             </a>
             <p className='course_description' >{props.name}</p>
+            <p className='course_description organizer' >{props.organizer}</p>
         </div></>
     )
 };
