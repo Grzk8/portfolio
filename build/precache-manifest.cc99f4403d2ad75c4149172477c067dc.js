4af3be3797393179386529c558f8ec0a3105d052
Grzegorz Kaczor <grzesiek125@op.pl>
2023-02-06 21:40:03 +0100

    build

---
+++ build/precache-manifest.cc99f4403d2ad75c4149172477c067dc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21be481a7e1f07dc60505a8654b78f23",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "630c4c0e54d6bf875f22",
+    "url": "/portfolio/static/css/main.03758c5b.chunk.css"
+  },
+  {
+    "revision": "c666c8628c199370395f",
+    "url": "/portfolio/static/js/2.1f5e315f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.1f5e315f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630c4c0e54d6bf875f22",
+    "url": "/portfolio/static/js/main.08b21575.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9582212abaa9e240f5168f0958820545",
+    "url": "/portfolio/static/media/1.9582212a.jpg"
+  },
+  {
+    "revision": "45c10bb3153ec635593482e9128eb9d0",
+    "url": "/portfolio/static/media/2.45c10bb3.jpg"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "3c8b72516080053e4d675d5abb8e7398",
+    "url": "/portfolio/static/media/fascinate-regular-webfont.3c8b7251.woff2"
+  },
+  {
+    "revision": "c14af0a4d79575883a8497c083a32e13",
+    "url": "/portfolio/static/media/fascinate-regular-webfont.c14af0a4.woff"
+  },
+  {
+    "revision": "9898c37489052ad7d42931336e1ed71f",
+    "url": "/portfolio/static/media/reeniebeanie-regular.9898c374.woff2"
+  },
+  {
+    "revision": "9f449072e5d72109ea70eb0f3bf5b734",
+    "url": "/portfolio/static/media/reeniebeanie-regular.9f449072.woff"
+  }
+]);
