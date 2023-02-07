ef37405c3d1fdb1be337a8774a1c66e8d66759ab
Grzegorz Kaczor <grzesiek125@op.pl>
2023-02-07 14:35:23 +0100

    media fonts chamged

---
+++ docs/precache-manifest.449a9ba5cc6442f5c8a5838487740d27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3499e77c60acea7a37be8f0ee5dcc9ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "db5ddce6a8ec922d7928",
+    "url": "/portfolio/static/css/main.a8a8067a.chunk.css"
+  },
+  {
+    "revision": "c666c8628c199370395f",
+    "url": "/portfolio/static/js/2.1f5e315f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.1f5e315f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5ddce6a8ec922d7928",
+    "url": "/portfolio/static/js/main.3a69c8e5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9582212abaa9e240f5168f0958820545",
+    "url": "/portfolio/static/media/1.9582212a.jpg"
+  },
+  {
+    "revision": "45c10bb3153ec635593482e9128eb9d0",
+    "url": "/portfolio/static/media/2.45c10bb3.jpg"
+  }
+]);
