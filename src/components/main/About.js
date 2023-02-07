103fefb72e90c0b60672464eac8354a60831d455
Grzegorz Kaczor <grzesiek125@op.pl>
2023-02-07 16:19:08 +0100

    photo width & padding

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -5,8 +5,8 @@ const About = () => {
     return <>
         <section className={'about'} id='about'>
             <div className={'about_container'}>
+            <img src={zdjecie} p className='about_container-photo'></img>
                 <p className='about_container-text header-text'>ABOUT ME</p>
-                <img src={zdjecie} p className='about_container-photo'></img>
                 <p className='about_container-text'>
                     I`m a web developer. Programming is my passion and I`m working on front-end technologies like JavaScript and React. At the same time I`m also diving into back-end technologies NodeJS, SQL, MongoDB; building full stack applications. I would like to develop my skills learning new technologies and expand the horizons, so I`m looking for the first professional experience that will enable me to do this.
                 </p>
